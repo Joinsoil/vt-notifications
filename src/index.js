3b288ad746e547eaa85dff5f38840f7b8623292f
Rich Rudzinski <rich@tragic.media>
2021-02-22 22:02:24 -0800

    path troubleshoot

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const path = require('path');
-path.resolve(__dirname, './dist')
-const VTNotification = require(path.resolve(__dirname, "./VTNotification.vue"));
-const VTNotificationGroup = require(path.resolve(__dirname, "./VTNotificationGroup.vue"));
-const methods = require(path.resolve(__dirname, './notifications.js'));
+const VTNotification = require(path.resolve(__dirname, "/VTNotification.vue"));
+const VTNotificationGroup = require(path.resolve(__dirname, "/VTNotificationGroup.vue"));
+const methods = require(path.resolve(__dirname, '/notifications.js'));
 
 let installed = false;
 
